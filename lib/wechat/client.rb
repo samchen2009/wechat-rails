fcb7deae346b070157755ef3ebf12a7800b43082
samchen2014 <sam.chen@ibluetag.com>
2015-10-08 17:59:05 +0800

    enable to return resp with post

--- lib/wechat/client.rb
+++ lib/wechat/client.rb
@@ -9,31 +9,46 @@ module Wechat
       @base = base
     end
 
-    def get path, header={}
-      request(path, header) do |url, header|
+    def get path, header={}, component_id=nil
+      request_with_reply(path, header) do |url, header|
         RestClient.get(url, header)
       end
     end
 
-    def post path, payload, header = {}
+    def post path, payload, header = {}, component_id = nil
       request(path, header) do |url, header|
-        RestClient.post(url, payload, header)
+        resp = RestClient.post(url, payload, header)
       end
     end
 
+    def post_with_reply path, payload, header = {}, component_id = nil
+      request_ex(path, header, true) do |url, header|
+        resp = RestClient.post(url, payload, header)
+      end
+    end
+
+    def request_with_reply path, header={}, &block
+      request_ex path, header, true, &block
+    end
+    
     def request path, header={}, &block
+      request_ex path, header, false, &block
+    end
+
+    def request_ex path, header={}, with_reply, &block
       url = "#{header.delete(:base) || self.base}#{path}"
       as = header.delete(:as)
       header.merge!(:accept => :json)
+
       response = yield(url, header)
 
-      raise "Request not OK, response code #{response.code}" if response.code != 200
+      raise "Request not OK, response code #{response.code}" if response.code >=400
       parse_response(response, as || :json) do |parse_as, data|
         break data unless (parse_as == :json && data["errcode"].present?)
 
         case data["errcode"]
         when 0 # for request didn't expect results
-          true
+          with_reply ? response : true
 
         when 42001, 40014 #42001: access_token超时, 40014:不合法的access_token
           raise AccessTokenExpiredError
